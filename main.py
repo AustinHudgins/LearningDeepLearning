9c20c7bef699c71a6d6725c5b99c70329ba795d2
Austin Hudgins <49458787+AustinHudgins@users.noreply.github.com>
2022-04-04 09:09:24 -0400

    update to main.py
    testing pulls

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('this is a edit on github.com to test the pulling')
 
 
 # Press the green button in the gutter to run the script.
